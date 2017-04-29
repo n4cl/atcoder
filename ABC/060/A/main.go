6b9859d34905538fe07fa84e0f166d7a0c69adc7
N4CL <devn4cl@gmail.com>
2017-04-29 23:58:32 +0900

    Add main.go in ABC/060/A

---
+++ ABC/060/A/main.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"bufio"
+	"os"
+	"strings"
+	"fmt"
+)
+
+var sc = bufio.NewScanner(os.Stdin)
+
+func main() {
+	sc.Scan()
+	s := sc.Text()
+	e := strings.Split(s, " ")
+	a, b, c := e[0], e[1], e[2]
+
+	if (a[len(a)-1] == b[0]) && (b[len(b)-1] == c[0])  {
+		fmt.Println("YES")
+	} else {
+		fmt.Println("NO")
+	}
+}
