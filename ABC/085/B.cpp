b98178b1e2d53cd1d22f6b30c2074eb2f65bc31b
n4cl <devn4cl@gmail.com>
2018-02-14 21:18:22 +0900

    Add B.cpp in ABC/085

---
+++ ABC/085/B.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <set>
+
+using namespace std;
+
+int main(void) {
+    int n, a;
+    set<int> s;
+    cin >> n;
+    for (int i=0;i<n;i++) {
+        cin >> a;
+        s.insert(a);
+    }
+    cout << s.size() << endl;
+    return 0;
+}
