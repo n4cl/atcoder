4f8e9177f09cfe8abee3564a04c9b75def363c90
n4cl <devn4cl@gmail.com>
2018-02-15 23:52:12 +0900

    Add A.cpp in ABC/079

---
+++ ABC/079/A.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main(void) {
+    string n;
+    cin >> n;
+
+    if ((n[0]==n[1]&&n[1]==n[2])||(n[1]==n[2]&&n[2]==n[3])) {
+        cout << "Yes" << endl;
+    } else {
+        cout << "No" << endl;
+    }
+    return 0;
+}
