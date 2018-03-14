c1fd6e412619af959b9bf0c127cb33e6a6b5e68c
n4cl <devn4cl@gmail.com>
2018-03-14 23:45:52 +0900

    Add B.cpp in ABC/049

---
+++ ABC/049/B.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define REP(i, n) for(int i = 0;i < n;i++)
+#define REPR(i, n) for(int i = n;i >= 0;i--)
+#define FOR(i, m, n) for(int i = m;i < n;i++)
+#define FORR(i, m, n) for(int i = m;i >= n;i--)
+
+int dx[4]={1, -1, 0, 0};
+int dy[4]={0, 0, 1, -1};
+
+int main(void) {
+    int h, w;
+    string s;
+    cin >> h >> w;
+
+    REP (i, h) {
+        cin >> s;
+        cout << s << endl;
+        cout << s << endl;
+    }
+    return 0;
+}
