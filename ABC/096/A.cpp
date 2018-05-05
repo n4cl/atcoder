5622add8046232e53b48031ebdd237b9ad638478
n4cl <devn4cl@gmail.com>
2018-05-05 23:08:24 +0900

    Add A.cpp in ABC/096

---
+++ ABC/096/A.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define REP(i, n) for(int i = 0;i < n;i++)
+#define REPR(i, n) for(int i = n;i >= 0;i--)
+#define FOR(i, m, n) for(int i = m;i < n;i++)
+#define FORR(i, m, n) for(int i = m;i >= n;i--)
+
+int dx[4]={1, -1, 0, 0};
+int dy[4]={0, 0, 1, -1};
+
+int main(void) {
+    int m, d, ans;
+    ans = 0;
+    cin >> m >> d;
+    ans = m;
+    if(d<m) ans--;
+    cout << ans << endl;
+    return 0;
+}
