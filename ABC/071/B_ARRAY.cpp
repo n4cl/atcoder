230c09b39f8da0470f721cedbf551b39648c064e
n4cl <devn4cl@gmail.com>
2018-05-08 22:17:28 +0900

    Add B_ARRAY.cpp in ABC/071
    Arrayを利用するとsizeで要素数が取れる

---
+++ ABC/071/B_ARRAY.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define REP(i, n) for(int i = 0;i < n;i++)
+#define REPR(i, n) for(int i = n;i >= 0;i--)
+#define FOR(i, m, n) for(int i = m;i < n;i++)
+#define FORR(i, m, n) for(int i = m;i >= n;i--)
+
+int dx[4]={1, -1, 0, 0};
+int dy[4]={0, 0, 1, -1};
+
+int main(void) {
+    string s;
+    array<int, 26> w={0};
+    cin >> s;
+    REP(i, s.size()){
+        w[s[i]-'a'] = 1;
+    }
+    REP(i, w.size()){
+        if(w[i]==0){
+            cout << char(i+'a') << endl;
+            return 0;
+        }
+    }
+    cout << "None" << endl;
+    return 0;
+}
