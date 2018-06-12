69d9d027862f6d9788cdf48232787e6360a17bb4
n4cl <devn4cl@gmail.com>
2018-06-12 19:01:36 +0900

    Add A.cpp in ABC/099

---
+++ ABC/099/A.cpp
@@ -0,0 +1,10 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(void) {
+    int a;
+    cin >> a;
+    if(a <= 999) cout << "ABC" << endl;
+    else cout << "ABD" << endl;
+    return 0;
+}
