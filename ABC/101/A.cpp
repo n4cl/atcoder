dd661c70fa5dcb6c24e989314b6911f1df78579f
n4cl <devn4cl@gmail.com>
2018-06-23 21:16:14 +0900

    Add A.cpp in ABC/101

---
+++ ABC/101/A.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(void) {
+    int ans;
+    ans = 0;
+    string s;
+    cin >> s;
+    for(int i = 0; i < s.size(); i++){
+        if(s[i] == '+') ans++;
+        else ans--;
+    }
+    cout << ans << endl;
+    return 0;
+}
