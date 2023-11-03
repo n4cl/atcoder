154e7df335a03b0766bc519b333d151a12448e08
n4cl <devn4cl@gmail.com>
2023-11-03 21:30:38 +0900

    Update: ABC/326/A

--- ABC/326/A.py
+++ ABC/326/A.py
@@ -1,13 +1,7 @@
 x, y = map(int, input().split())
 
-if x < y:
-    if y - x > 2:
-        ans = "No"
-    else:
-        ans = "Yes"
+if -3 <= y - x <= 2:
+    ans = "Yes"
 else:
-    if x - y > 3:
-        ans = "No"
-    else:
-        ans = "Yes"
+    ans = "No"
 print(ans)
